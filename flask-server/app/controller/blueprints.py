0b348f75dd43c8dd1c302d8b116487d2459a7973
Ria Banerjee <riabanerjee@Rias-MacBook-Air.local>
2026-02-01 13:38:27 -0700

    workflow check

--- flask-server/app/controller/blueprints.py
+++ flask-server/app/controller/blueprints.py
@@ -3,6 +3,7 @@ from app.routes.information_route import info_bp
 from app.routes.health_route import health_bp
 from app.routes.chatbot_route import chatbot_bp
 
+#Registering blueprints
 def register_blueprints(app):
     app.register_blueprint(email_bp)
     app.register_blueprint(health_bp)
