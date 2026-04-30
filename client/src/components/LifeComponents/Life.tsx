6b5a5219cf6239ddaae62ae58efed07748325d48
Ria Banerjee <riabanerjee@Rias-MacBook-Air.local>
2026-04-29 22:41:23 -0600

    updates

--- client/src/components/LifeComponents/Life.tsx
+++ client/src/components/LifeComponents/Life.tsx
@@ -3,31 +3,32 @@ import Rochelle from '../../assets/images/Rochelle.jpeg';
 import iit from '../../assets/images/iit.jpeg'
 import Google_crowdsource from '../../assets/images/Google_Crowdsource.jpg'
 import RA from '../../assets/images/RA.jpeg'
-import Visamo from '../../assets/images/Visamo.jpeg'
 import Guitar from '../../assets/images/Guitar.jpeg'
+import AIDev from '../../assets/images/AIDev.jpg'
 
 const images = [
-  { src: iit, alt: 'Riaprofile', tag:"IIT Gandhinagar and ACM-Women's Chapter, India",desc:"Stood 1st for Project Presentation"},
-  { src: Google_crowdsource, alt: 'AU',tag:"Google Crowdsource", desc:"Volunteered for Speech Training" },
-  { src: RA, alt: 'Rochelle',tag:"UNC Charlotte",desc:"Research participant for Anxiety Reduction via VR environments" },
-  { src: Visamo, alt: 'Rochelle',tag:"Conducted Art class in local orphanage",desc:"" },
-  { src: Rochelle, alt: 'Rochelle',tag:"Cat Mom",desc:"Adopt, don't shop!" },
-  { src: Guitar, alt: 'Rochelle',tag:"Passtime Guitar Player",desc:"" },
-  
+  { src: AIDev, alt: 'AIDev Attendee', tag: "AI Dev x SF by Deeplearning.ai 2026", desc: "Keepin' up with Community" },
+  { src: iit, alt: 'Riaprofile', tag: "IIT Gandhinagar and ACM-Women's Chapter, India", desc: "Stood 1st for Project Presentation" },
+  { src: Google_crowdsource, alt: 'AU', tag: "Google Crowdsource", desc: "Volunteered for Speech Training" },
+  { src: RA, alt: 'Research Participation', tag: "UNC Charlotte", desc: "Research participant for Anxiety Reduction via VR environments" },
+
+  { src: Rochelle, alt: 'Rochelle', tag: "Cat Mom", desc: "Adopt, don't shop!" },
+  { src: Guitar, alt: 'Guitar', tag: "Passtime Guitar Player", desc: "" },
+
   // Add more images as needed
 ];
 
 function Life() {
   return (
     <>
-    <div className="text-3xl sm:text-4xl md:text-5xl font-bold text-center" style={{color:'#BBBBBB'}}>Life Outside Of Work</div>
-    <div className="flex min-h-screen w-full mt-20 justify-center items-center">
+      <div className="text-3xl sm:text-4xl md:text-5xl font-bold text-center" style={{ color: '#BBBBBB' }}>Life Outside Of Work</div>
+      <div className="flex min-h-screen w-full mt-20 justify-center items-center">
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-20">
           {images.map((image, index) => (
             <div
               key={index}
-              className="w-60 shadow-md rounded-md p-3 transition-transform hover:scale-105 overflow-hidden flex flex-col border-3 border-[#735F32]" 
-              style={{color:'#000000'}} // Added flex flex-col
+              className="w-60 shadow-md rounded-md p-3 transition-transform hover:scale-105 overflow-hidden flex flex-col border-3 border-[#735F32]"
+              style={{ color: '#000000' }} // Added flex flex-col
             >
               <div className="relative pb-50 flex-grow"> {/* This will make the image container more horizontally tall and allow input to stick to the bottom */}
                 <img
@@ -36,8 +37,8 @@ function Life() {
                   className="w-full h-full object-cover mb-3 mx-auto"  // Ensure the image fills the container properly
                 />
               </div>
-              <div className="w-full p-2 rounded-md text-lg mt-auto" style={{color:'#BBBBBB'}}>
-                <b>{image.tag} </b>{image.desc} 
+              <div className="w-full p-2 rounded-md text-lg mt-auto" style={{ color: '#BBBBBB' }}>
+                <b>{image.tag} </b>{image.desc}
               </div>
             </div>
           ))}
@@ -46,6 +47,6 @@ function Life() {
     </>
   );
 
-  }
+}
 
 export default Life;
