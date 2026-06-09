b7eb6fc5d507c47b3b23b7e3082183d82f29a546
Ria Banerjee <riabanerjee@Rias-MacBook-Air.local>
2026-06-08 23:13:07 -0600

    removing unwanted files

--- terraform/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "portfolio-terraform-state-535002893779"
-    key            = "portfolio/terraform.tfstate"
-    region         = "us-east-1"
-    use_lockfile   = true
-    encrypt        = true
-  }
-}
